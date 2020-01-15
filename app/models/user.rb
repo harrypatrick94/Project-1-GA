eeea37e65f79ad05a2721cf2a7ac64e5419e33f8
Harry Patrick <harry.g.patirck94@gmail.com>
2020-01-15 11:33:01 +1100

    heroku deployment with cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   after_create :give_user_default_genres
 
   def give_user_default_genres
-    rasie "hell"
-     give the new user some default values
+    # rasie "hell"
+    # give the new user some default values
     # current_user << jazz << blues << rock
 
   end
